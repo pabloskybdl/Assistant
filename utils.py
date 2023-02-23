bb027131f973608a40e65321bcff9339b7638195
pablo.itb <pabloskybdl@itb.cat>
2023-02-23 20:42:55 +0100

    Some default senteces

---
+++ utils.py
@@ -0,0 +1,12 @@
+# region Open Sentences
+opening_text = [
+    "Genial, estoy en ello señor.",
+    "Enseguida me pongo con ello señor",
+    "Okay señor, trabajaré en ello.",
+    "Deme un segundo señor.",
+    "A su servicio, señor",
+    "Porsupuesto, señor",
+    "Enseguida señor"
+    "Como desee"
+]
+# endregion
